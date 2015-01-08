4b6d256f8a2ef3bc474916454af8cce1165d1679
Shin1 Kashimura <in-async@users.noreply.github.com>
2015-01-08 14:14:05 +0900

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 $('div.remodal-content').eq(1)
 .scroll(function (e) {
     var top = $(this).scrollTop();
-    $('.unframe-btn').text(top);
+//    $('.unframe-btn').text(top);
     e.preventDefault();
     return false;
 })
