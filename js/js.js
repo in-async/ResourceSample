a318bd31ad49bf59fe67a19f66cc794753303997
Shin1 Kashimura <in-async@users.noreply.github.com>
2015-01-17 18:08:44 +0900

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,39 +1,7 @@
-/*
-$('div.remodal-content').eq(1)
-.scroll(function (e) {
-    var top = $(this).scrollTop();
-//    $('.unframe-btn').text(top);
-    
-    var prevTop = $(this).data('prevScrollTop');
-    if (typeof prevTop === 'undefined') {
-        $(this).data('prevScrollTop', top);
-        return;
-    }
-
-    if (prevTop > 100 && top == 0) {
-        $(this).scrollTop(prevTop);
-    } else {
-        $(this).data('prevScrollTop', top);
-    }
-})
-;
-//$('div.remodal-content iframe, document, window')
-$('document, window, body')
-.on('touchmove, touchstart, touchend', function (e) {
-//    alert('touchmove');
-    var top = $(this).scrollTop();
-    $('.unframe-btn').text('touchmove: ' + top);
-})
-;
-$('div.remodal-content').on('resize', function(e) {
-    alert('resize');
-})
-*/
 var isIOS = /iP(hone|od|ad)/.test(navigator.platform);
 if (isIOS) {
     var prevIframeHeight = 10000;
     var intervalId = setInterval(function() {
-        console.log(intervalId);
         $iframe = $('div.remodal-content iframe')
         var iframeHeight = $iframe.height();
         if (iframeHeight > prevIframeHeight) {
@@ -53,29 +21,16 @@ if (isIOS) {
         $remodalContent.data('prev-scroll-top', top);
     //    alert(top);
         
+        // iframe.height 最適化
+        $html = $('html').css('overflow', 'hidden');
         $(this).css('height', 'auto');
-        
         var height = $(this).height();
         if (height > 0) {
-            if (height < 200) {
-                // 元に戻す
-                $(this).css('height', '');
-            } else {
-                $(this).css('height', (height + 50) + 'px');
-            }
+            $(this).height(height + 50);
+            $html = $('html').css('overflow', '');
             $remodalContent.scrollTop(top);
             
             $('.unframe-btn').text('height: ' + height);    // for debug
         }
     });
 }
-/*
-$('div.remodal-iframe div.remodal-content').on('scroll', function (e) {
-    var prevTop = $(this).data('prev-scroll-top');
-    if (typeof prevTop !== 'undefined') {
-        alert(prevTop);
-        $(this).scrollTop(prevTop);
-        $(this).removeData('prev-scroll-top');
-    }
-});
-*/
